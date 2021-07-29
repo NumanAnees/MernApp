e4fcd24f57c552557fe04c4e423bba61ccf9f104
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2021-07-30 04:23:46 +0500

    routes updated

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ app.get("/About", (req, res) => {
 app.get("/Contact", (req, res) => {
   res.send("hello from the contact");
 });
+app.get("/Signin", (req, res) => {
+  res.send("hello from the Signin");
+});
+app.get("/Singup", (req, res) => {
+  res.send("hello from the Singnup");
+});
+
 app.listen(3000, () => {
   console.log("server is running at port 3000");
 });
