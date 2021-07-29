712791f04cbe050167b8d68dccd74938c0a8ecfe
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2021-07-30 04:21:57 +0500

    different routes

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hello");
+app.get("/About", (req, res) => {
+  res.send("hello from the about");
+});
+app.get("/Contact", (req, res) => {
+  res.send("hello from the contact");
 });
 app.listen(3000, () => {
   console.log("server is running at port 3000");
