8376d6c0290b7a8bcc6afd6f8e698f8b2e678dde
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2021-07-30 22:29:43 +0500

    1st user created

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,11 +1,41 @@
 const express = require("express");
 const router = express.Router();
+require("../DB/connection");
+const User = require("../Models/userSchema");
 
 router.post("/register", (req, res) => {
-  console.log(req.body);
-  res.json({
-    message: req.body,
-  });
+  const { name, email, phone, work, password, cpassword } = req.body;
+  if (!name || !email || !phone || !work || !password || !cpassword) {
+    return res.status(422).json({
+      error: "plz fill all the fields",
+    });
+  }
+  User.findOne({ email: email })
+    .then((response) => {
+      if (response) {
+        return res.status(422).json({
+          message: "User already exist",
+        });
+      }
+      const user = new User({ name, email, phone, work, password, cpassword });
+      user
+        .save()
+        .then(() => {
+          res.status(201).json({
+            message: "User created successfully",
+          });
+        })
+        .catch((e) => {
+          res.status(500).json({
+            error: "Failed to register",
+          });
+        });
+    })
+    .catch((e) => {
+      res.status(500).json({
+        error: "Failed to register",
+      });
+    });
 });
 
 module.exports = router;
