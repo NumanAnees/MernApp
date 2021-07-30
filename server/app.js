ba482326fa3efce5e0bbe16e7cbac21ed1cd101b
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2021-07-30 20:06:09 +0500

    seprate directory made for handling db schemas and connections

--- server/app.js
+++ server/app.js
@@ -1,23 +1,11 @@
-const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const express = require("express");
 const app = express();
+
 dotenv.config({ path: "./config.env" });
+require("./DB/connection");
 const PORT = process.env.PORT;
-const DB = process.env.DB;
-mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-  })
-  .then(() => {
-    console.log("connection successfull");
-  })
-  .catch((e) => {
-    console.log(e);
-  });
+
 app.get("/About", (req, res) => {
   res.send("hello from the about");
 });
