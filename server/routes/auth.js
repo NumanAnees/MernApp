b90b78c87ea028637172b5aed21f32cdbb2bc4aa
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2021-07-31 05:58:56 +0500

    refactoring the code

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,39 +3,38 @@ const router = express.Router();
 require("../DB/connection");
 const User = require("../Models/userSchema");
 
-router.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
   const { name, email, phone, work, password, cpassword } = req.body;
   if (!name || !email || !phone || !work || !password || !cpassword) {
     return res.status(422).json({
       error: "plz fill all the fields",
     });
   }
-  User.findOne({ email: email })
-    .then((response) => {
-      if (response) {
-        return res.status(422).json({
-          message: "User already exist",
-        });
-      }
-      const user = new User({ name, email, phone, work, password, cpassword });
-      user
-        .save()
-        .then(() => {
-          res.status(201).json({
-            message: "User created successfully",
-          });
-        })
-        .catch((e) => {
-          res.status(500).json({
-            error: "Failed to register",
-          });
-        });
-    })
-    .catch((e) => {
-      res.status(500).json({
-        error: "Failed to register",
+
+  try {
+    const response = await User.findOne({ email: email });
+
+    if (response) {
+      return res.status(422).json({
+        message: "User already exist",
       });
+    }
+    const user = new User({
+      name,
+      email,
+      phone,
+      work,
+      password,
+      cpassword,
     });
+    await user.save();
+
+    res.status(201).json({
+      message: "User created successfully",
+    });
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 module.exports = router;
