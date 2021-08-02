25f7901f7a21a50d103fcd582771a7fc07c52733
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2021-08-02 08:45:50 +0500

    signup page created

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./App.css";
 import { Route } from "react-router-dom";
 import Navbar from "./components/navbar";
 import Home from "./components/home";
--- client/src/components/signup.js
+++ client/src/components/signup.js
@@ -1,9 +1,93 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const signup = () => {
   return (
-    <div>
-      <h1>signup</h1>
+    <div className="holder">
+      <div className="parent">
+        <h3>Sign Up</h3>
+        <form action="">
+          <div>
+            <input
+              type="text"
+              placeholder="Name *"
+              name="name"
+              id="name"
+              required
+            />
+            <span className="border"></span>
+          </div>
+          <div>
+            <input
+              type="email"
+              placeholder="Email *"
+              name="email"
+              id="email"
+              required
+            />
+            <span className="border"></span>
+          </div>
+          <div>
+            <input
+              type="work"
+              placeholder="work *"
+              name="work"
+              id="work"
+              required
+            />
+            <span className="border"></span>
+          </div>
+          <div>
+            <input
+              type="phone"
+              placeholder="phone *"
+              name="phone"
+              id="phone"
+              required
+            />
+            <span className="border"></span>
+          </div>
+          <div>
+            <input
+              type="password"
+              placeholder="Password *"
+              name="password"
+              id="password"
+              required
+            />
+            <span className="border"></span>
+          </div>
+          <div>
+            <input
+              type="password"
+              placeholder="Confirm Password *"
+              name="cpassword"
+              id="cpassword"
+              required
+            />
+            <span className="border"></span>
+          </div>
+          <div className="form-group form-button">
+            <input
+              type="submit"
+              name="signup"
+              id="signup"
+              className="form-submit"
+              value="register"
+            />
+          </div>
+          <p>
+            Have Account?
+            <NavLink to="/signin">Login Here</NavLink>
+          </p>
+        </form>
+        <h4>Or Register With</h4>
+        <div className="social">
+          <i className="fab fa-facebook-f"></i>
+          <i className="fab fa-twitter"></i>
+          <i className="fab fa-google-plus-g"></i>
+        </div>
+      </div>
     </div>
   );
 };
